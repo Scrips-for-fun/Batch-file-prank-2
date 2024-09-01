3319a1468af802cd6ae6c4756797c1e20efd91ba
Scrips-for-fun <jamescyran@hotmail.com>
2024-09-01 19:13:09 -0400

    Add files via upload

---
+++ Cool-Program.bat
@@ -0,0 +1,18 @@
+@echo off
+:start
+start "" cmd.exe
+color a
+echo GET PRANKED!!!
+echo GET PRANKED!!!
+echo GET PRANKED!!!
+echo GET PRANKED!!!
+echo GET PRANKED!!!
+echo GET PRANKED!!!
+echo GET PRANKED!!!
+echo GET PRANKED!!!
+echo GET PRANKED!!!
+echo GET PRANKED!!!
+echo GET PRANKED!!!
+echo GET PRANKED!!!
+echo GET PRANKED!!!
+goto start
